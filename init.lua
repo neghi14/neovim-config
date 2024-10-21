5f1b4bf26f0f682d3b5892d89e891dd422e4ef32
neghi <me@neghi.dev>
2024-10-21 17:03:00 +0100

    update: added color scheme

--- init.lua
+++ init.lua
@@ -1,1 +1,25 @@
+vim.cmd("set tabstop=2")
+vim.cmd("let g:loaded_perl_provider= 0")
+vim.cmd("let g:loaded_python3_provider= 0")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+				vim.fn.system({
+								"git",
+								"clone",
+								"--filter=blob:none",
+								"https://github.com/folke/lazy.nvim.git",
+								"--branch=stable",
+								lazypath,
+				})
+end
+vim.opt.rtp:prepend(lazypath)
 
+local plugins= {
+				{"catppuccin/nvim", name = "catppuccin", priority = 1000},
+}
+local opts= {}
+
+require("lazy").setup(plugins, opts)
+require("catppuccin").setup()
+
+vim.cmd.colorscheme "catppuccin"
