6949f844bbef8c4662292042b0aab8426a4fcfdc
neghi <me@neghi.dev>
2024-10-21 19:12:07 +0100

    update: updated plugin arrangement

--- init.lua
+++ init.lua
@@ -1,46 +1,19 @@
-vim.cmd("set tabstop=2")
 vim.cmd("let g:loaded_perl_provider= 0")
 vim.cmd("let g:loaded_python3_provider= 0")
 vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-				vim.fn.system({
-								"git",
-								"clone",
-								"--filter=blob:none",
-								"https://github.com/folke/lazy.nvim.git",
-								"--branch=stable",
-								lazypath,
-				})
+	vim.fn.system({
+		"git",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+		})
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins= {
-				{"catppuccin/nvim", name = "catppuccin", priority = 1000},
-				{"nvim-telescope/telescope.nvim", tag = "0.1.8", dependencies = {"nvim-lua/plenary.nvim"}},
-				{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate", config = function ()
-								local configs = require("nvim-treesitter.configs")
+require("lazy").setup("plugins")
 
-								configs.setup({
-								ensure_installed = {"c", "go", "lua", "make", "dockerfile", "html", "javascript", "json", "svelte", "tsx", "typescript", "vim", "vimdoc", "yaml"},
-								sync_install = false,
-								highlight = {enable = true},
-								indent = {enable = true},
-								})
-				end
-				},
-				
-}
 
-local opts= {}
-
-require("lazy").setup(plugins, opts)
-require("catppuccin").setup()
-
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>f", builtin.find_files, {})
-vim.keymap.set("n", "<leader>b", builtin.buffers, {})
-vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
-
-vim.cmd.colorscheme "catppuccin"
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return 	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate", 
+		config = function ()
+			local configs = require("nvim-treesitter.configs")
+
+			configs.setup({
+				ensure_installed = {"c", "go", "lua", "make", "dockerfile", "html", "javascript", "json", "svelte", "tsx", "typescript", "vim", "vimdoc", "yaml"},
+				sync_install = false,
+				highlight = {enable = true},
+				indent = {enable = true},
+			})
+		end
+	}
+
