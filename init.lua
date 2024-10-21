a2d04924affd0d6a9939a532691f68e8e0ecabea
neghi <me@neghi.dev>
2024-10-21 17:46:34 +0100

    updated: added telescope for file search

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 vim.cmd("set tabstop=2")
 vim.cmd("let g:loaded_perl_provider= 0")
 vim.cmd("let g:loaded_python3_provider= 0")
+vim.g.mapleader = " "
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 				vim.fn.system({
@@ -16,10 +18,18 @@ vim.opt.rtp:prepend(lazypath)
 
 local plugins= {
 				{"catppuccin/nvim", name = "catppuccin", priority = 1000},
+				{"nvim-telescope/telescope.nvim", tag = "0.1.8", dependencies = {"nvim-lua/plenary.nvim"}},
+				
 }
+
 local opts= {}
 
 require("lazy").setup(plugins, opts)
 require("catppuccin").setup()
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>f", builtin.find_files, {})
+vim.keymap.set("n", "<leader>b", builtin.buffers, {})
+vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
 
 vim.cmd.colorscheme "catppuccin"
