e377ead318b4b5bbfbb638cb71af095f4fbcd5bb
neghi <me@neghi.dev>
2024-10-22 10:11:23 +0100

    update: added autocomplete and gitsigns plugin

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,8 +1,22 @@
+-- Disable unused language providers
+
 vim.cmd("let g:loaded_perl_provider= 0")
 vim.cmd("let g:loaded_python3_provider= 0")
+
+-- Set tab width
 
 vim.cmd("set tabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set expandtab")
+
+-- Display relative numbers
+
 vim.cmd("set number")
 vim.cmd("set rnu")
+
+-- Set list Char
+vim.opt.list = true
+vim.opt.listchars = { tab = ">> ", trail = ">" }
+
+-- Enable nerd font
+vim.g.have_nerd_font = true
---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,13 @@
+return {
+	"windwp/nvim-autopairs",
+	event = "InsertEnter",
+	config = function()
+		require("nvim-autopairs").setup({
+			disable_filetype = { "TelescopePrompt", "spectre_panel" },
+			disable_in_replace_mode = true,
+			disable_in_visualblock = false,
+			enable_afterquote = true,
+			enable_bracket_in_quote = true,
+		})
+	end,
+}
