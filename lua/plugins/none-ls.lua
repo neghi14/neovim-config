1f087497cf988e474d5371bd58eb78eca6721890
neghi14 <sambasseynengimote@gmail.com>
2025-01-13 18:08:59 +0100

    replaced >> for tabs, it was ugly

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,6 +8,7 @@ return {
                 null_ls.builtins.formatting.prettier,
                 null_ls.builtins.diagnostics.golangci_lint,
                 null_ls.builtins.formatting.pint,
+                null_ls.builtins.formatting.blade_formatter,
             },
         })
 
